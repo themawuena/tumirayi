0cac416065607c4d20a7f0bb7a5f07cff630d7a4
imoempire <mrimoempire@gmail.com>
2024-07-26 08:52:45 +0000

    add loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Center, Flex, Text } from "@mantine/core";
+import { Center, Flex, Loader, Text } from "@mantine/core";
 import { useSession } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -8,7 +8,6 @@ export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
- 
   // if (status === "loading") {
   //   return (
   //     <Flex align={"center"} justify={"center"} style={{ height: '100vh' }}>
@@ -24,16 +23,18 @@ export default function Home() {
   // useEffect(() => {
   //   if (status !== "loading" && session) {
   //     redirect("/dashboard");
-  //   } 
+  //   }
   //    if (status === "unauthenticated") {
   //     redirect("/auth/signin");
   //   }
   // }, [session, status]);
 
-
   return (
     <Center style={{ height: "100vh" }}>
-      <Text c={"dark"}>Welcome Back</Text>
+      <Flex direction={"column"} gap={20} align={"center"} justify={"center"}>
+        <Text c={"dark"}>Welcome Back</Text>
+        <Loader size={50} />
+      </Flex>
     </Center>
   );
 }
